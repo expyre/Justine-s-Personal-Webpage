0ebfd24e9a208e9153667aa6aa2dc4534942dd69
expyre <kyoskej20@gmail.com>
2024-06-03 05:08:00 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const navbar = document.getElementById("navbar");
+const navLinks = navbar.querySelectorAll("a");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (window.pageYOffset >= sectionTop) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
+
+const textItems = document.querySelectorAll('.text-item');
+
+let currentIndex = 0;
+
+function rotateText() {
+  textItems[currentIndex].classList.remove('show');
+  textItems[currentIndex].classList.add('hide');
+
+  currentIndex = (currentIndex + 1) % textItems.length;
+
+  textItems[currentIndex].classList.remove('hide');
+  textItems[currentIndex].classList.add('show');
+
+  setTimeout(rotateText, 1500);
+}
+
+rotateText();
+
+
+
